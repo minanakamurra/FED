f0af7798a97e8ba82cca0a0ae1e40013ebfbe8cd
minanakamurra <55148384+minanakamurra@users.noreply.github.com>
2021-10-07 13:06:53 +0200

    Add files via upload

--- disneyland.js
+++ disneyland.js
@@ -35,30 +35,3 @@ slider.addEventListener("mousemove", e => {
   slider.scrollLeft = scrollLeft - walk;
 });
 
-
-// const slider2 = document.querySelector("section:nth-of-type(13) ul:first-of-type");
-// let isDown2 = false;
-// let startX2;
-// let scrollLeft2;
-
-// slider2.addEventListener("mousedown2", e => {
-//   isDown2 = true;
-//   slider2.classList.add("active2");
-//   startX2 = e.pageX - slider2.offsetLeft;
-//   scrollLeft2 = slider2.scrollLeft2;
-// });
-// slider2.addEventListener("mouseleave2", () => {
-//   isDown2 = false;
-//   slider2.classList.remove("active2");
-// });
-// slider2.addEventListener("mouseup2", () => {
-//   isDown2 = false;
-//   slider2.classList.remove("active2");
-// });
-// slider2.addEventListener("mousemove2", e => {
-//   if (!isDown2) return;
-//   e.preventDefault();
-//   const x2 = e.pageX - slider2.offsetLeft;
-//   const walk2 = x2 - startX;
-//   slider2.scrollLeft2 = scrollLeft2 - walk2;
-// });
